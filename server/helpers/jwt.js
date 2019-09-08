3a471dbd1c1aedd3a9115bff7de467ba42628dd1
jackbloo <a.iqbal1404@gmail.com>
2019-09-08 19:15:03 +0700

    Mohammad Iqbal Andikoputro

--- server/helpers/jwt.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken')
-const SECRET_JWT = process.env.JWT
-function generateJWTToken(data){
-    return jwt.sign({ data }, SECRET_JWT, { expiresIn: 10*60 })
-}
-module.exports = {
-    generateJWTToken
-}
--- server/routes/user.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const UserController = require('../controllers/user')
-router.post('/register', UserController.createUser)
-router.post('/loginForm', UserController.loginForm)
-router.post('/loginGoogle', UserController.loginGoogle)
-
-module.exports = router
